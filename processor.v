1ba8e918dbefbf244330eb8cf89922ebc99c8d5e
wallaceicy06 <seanharger@me.com>
2013-12-09 23:31:43 -0600

    Added initial implementation of pipeline registers 1 and 4

--- processor.v
+++ processor.v
@@ -1,3 +1,6 @@
+`define REG_ADDR_LEN 5
+`define MEM_WIDTH 32
+`define INSTR_BIT_LEN 32
 // Define Symbolic Names for Signals  and Register Fields
 
 
@@ -22,10 +25,16 @@ initial begin
 
 endmodule
 
-module pipeReg1(clk, pc, instruction);
+module pipeReg1(clk, pcIn, pcOut, instrIn, instrOut);
     input clk;
-    input [31:0] pc;
+    input [`INSTR_BIT_LEN - 1:0] pcIn, instrIn;
+    output reg [`INSTR_BIT_LEN - 1:0] pcOut, instrOut;
 
+    always @(posedge clk)
+    begin
+        pcOut <= pcIn;
+        instrOut <= instrIn;
+    end
 endmodule
 
 module pipeReg2();
@@ -34,5 +43,32 @@ endmodule
 module pipeReg3();
 endmodule
 
-module pipeReg4();
+module pipeReg4(clk,
+                wbEnableIn, wbEnableOut,
+                memReadIn, memReadOut,
+                memWriteIn, memWriteOut,
+                iTypeIn, iTypeOut,
+                isBranchIn, isBranchOut,
+                isJumpIn, isJumpOut,
+                dataIn, dataOut,
+                dstIn, dstOut);
+
+    input clk;
+    input wbEnableIn, memReadIn, memWriteIn, iTypeIn, isBranchIn, isJumpIn;
+    output reg wbEnableOut, memReadOut, memWriteOut, iTypeOut, isBranchOut, isJumpOut;
+    input [`MEM_WIDTH - 1:0] dataIn;
+    output reg [`MEM_WIDTH - 1:0] dataOut;
+    input [`REG_ADDR_LEN - 1:0] dstIn;
+    output reg [`REG_ADDR_LEN - 1:0] dstOut;
+
+    always @(posedge clk)
+    begin
+        wbEnableOut <= wbEnableIn;
+        memReadOut <= memReadIn;
+        memWriteOut <= memWriteIn;
+        iTypeOut <= iTypeIn;
+        isBranchOut <= isBranchIn;
+        isJumpOut <= isJumpIn;
+    end
+
 endmodule
