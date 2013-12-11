668c7aff169d1dc913a8c0173cc12faac1122197
ricomarquez <rmarquez93@gmail.com>
2013-12-10 22:33:30 -0600

    module documentation for each pipeReg

--- processor.v
+++ processor.v
@@ -23,15 +23,27 @@ initial begin
 endmodule
 
 module pipeReg1(clk, pc, instruction);
+    // The following are contained in this module:
+    // [31:0] pc
+    // [31:0] instruction
+    
     input clk;
     input [31:0] pc;
 
 endmodule
 
 module pipeReg2();
+    // The following are contained in this module:
+    // [31:0] pc
+    // [10:0] IDCntrl - aluSet, wSel, aluOp(this guy is 3 bits long), memWrite, rDataSel, rWrite, jmp, br
 endmodule
 
 module pipeReg3(wbEnable, memRead, memWrite, iType, isBranch, isJump, alures, rt);
+    // The following are contained in this module
+    // [3:0] ExCntrl - br, memWrite, rDataSel, rWrite
+    // [31:0] result
+    // [4:0] dst
+    // [31:0] ddr
     input wbEnable, memRead, memWrite, iType, isBranch, isJump;
     input [31:0] aluRes;
     input [4:0] inRT;
@@ -48,4 +60,8 @@ module pipeReg3(wbEnable, memRead, memWrite, iType, isBranch, isJump, alures, rt
 endmodule
 
 module pipeReg4();
+    // memCntrl (just write)
+    // [31:0] data
+    // [4:0] dst
+    
 endmodule
