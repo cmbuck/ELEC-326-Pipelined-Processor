7359a855c8d5b7dbfc91c9903e26c478135d701c
Christopher Buck <cmb15@rice.edu>
2013-12-13 14:25:52 -0600

    Added ALU and Decoder implementations

--- alu.v
+++ alu.v
@@ -1,17 +1,51 @@
 module alu(aluOp, operand1, operand2, jump, branch, pc, addrInfo, aluResult, targetAddr);
 
-   input wire  [31:0] operand1, operand2;
-   input wire [31:0]  pc;
-   input wire [25:0]  addrInfo;
-   input wire 	      jump, branch;
-   input wire [2:0]   aluOp;
-   output reg [31:0]  aluResult, targetAddr;
-  
+input wire  [31:0] operand1, operand2;
+input wire [31:0] pc;
+input wire [25:0] addrInfo;
+input wire jump, branch;
+input wire [2:0] aluOp;
 
-   // Use your alu module from Homework 3
+output reg [31:0] aluResult, targetAddr;
+
+   // Set "aluResult" to  the result of the ALU operation indicated by "aluOp".
+   // Set "targetAddr" if either "branch" or "jump" signal is  set.
    
-endmodule
+   // aluOP operations
+   // 1: Add operands.
+   // 2: Subtract operand2 from operand1.
+   // 3: Bitwise AND of operands.
+   // 4: Bitwise OR of operands.
+   // 5: Output set to TRUE if operand1 is less than operand2 interpreted as unsigned integers; else set to FALSE.
+   // 6: Output set to TRUE if operands are equal; else set to FALSE.
+   // 7: Output set to TRUE if operand1 is less than operand2 interpreted as signed integers; else set to FALSE.
+      
+always @(*)
+begin
+	case (aluOp)
+	3'h1:	aluResult = operand1 + operand2;
+	3'h2:	aluResult = operand1 - operand2;
+	3'h3:	aluResult = operand1 & operand2;
+	3'h4:	aluResult = operand1 | operand2;
+	3'h5:	aluResult = operand1 < operand2;
+	3'h6:	aluResult = operand1 == operand2;
+	3'h7:	aluResult = $signed(operand1) < $signed(operand2);
+	default:	aluResult = 32'hx;
+	endcase
 
+   // Target Address for Branch
+   //  Sign-extend the lower 16 bits of  "addrInfo", scale it by 4, and add to  "pc".  
+if (branch == 1'b1)
+	targetAddr = ({{16{addrInfo[15]} }, addrInfo[15:0] } << 2) + pc;
+
+   // Target Address for Jump
+   // The 26-bit "addrInfo" is scaled by 4 and concatenated to the 4 high-order bits of "pc". 
+else if (jump == 1'b1)
+	targetAddr = { pc[31:28], addrInfo << 2 };
+
+end
+
+endmodule
 
 
 
--- decoder.v
+++ decoder.v
@@ -1,14 +1,89 @@
+//  Suggestion:
+//  Define  macros like the following commented block. That alllows you to use  meangful symbolic names (rather than 
+// hard-to-parse and easy to  mangle) bit strings in the main code.
+
+
+//Macros
+//Defined as their opcodes
+`define RType 6'h0
+`define ADDI  6'h8
+`define	LOAD  6'h20
+`define	STORE 6'h30
+`define	BEQ   6'h4
+`define	JMP   6'h2
+
+//Defined as their funct values
+`define ADD  6'h20
+`define SUB  6'h22
+`define AND  6'h24
+`define  OR  6'h25
+`define SLT  6'h2A
+`define NOP  6'h0
+
+//Defined as part of instruction
+`define OPCODE	instruction[31:26]
+`define R_D		instruction[15:11]
+`define R_T		instruction[20:16]
+`define ADDRINFO	instruction[25:0]
+`define FUNCT	instruction[5:0]
+
+
+
 module insDecoder(instruction, addrInfo, ALUop, writeReg, memRead, memWrite, iType, wbEnable, isBranch, isJump);
 
+input wire [31:0] instruction;
+output wire [25:0] addrInfo;
+output reg [2:0] ALUop;
+output wire [4:0] writeReg;
+output wire wbEnable, memRead, memWrite, iType, isBranch, isJump;
+
+
+   /*
+    Input: 
+                   32-bit "instruction"
+    Outputs:
+    
+    isBranch, isJump : TRUE  if instruction is branch or jump respectively; FALSE  otherwise.
+    memRead: TRUE if instruction will read from data memory; FALSE otherwise.
+    memWrite: TRUE if instruction will write to  data memory; FALSE otherwise.
+    wbEnable: TRUE if instruction will write a result to a register; FALSE otherwise
+    iType: TRUE if it is an IType instruction that is not a Branch; FALSE for Branch and non I-Type istructions.
+    
+    writeReg: id of register to be written; dont care if wbEnable is set to FALSE.
+    addrInfo: the 26 LSBs of the instruction
+    ALUop:  Defined by Table 2/
    
-   input [31:0] instruction;
-   output [25:0] addrInfo;
-   output [2:0]  ALUop;
-   output [4:0]  writeReg;
-   output wbEnable, memRead, memWrite, iType, isBranch, isJump;
+    */
+	
+	assign isBranch = 	(`BEQ == `OPCODE);
+	assign isJump	 =	(`JMP == `OPCODE);
+	assign memRead	 =	(`LOAD == `OPCODE);
+	assign memWrite =	(`STORE == `OPCODE);
+	assign wbEnable =	(`OPCODE == `RType || `OPCODE == `ADDI || `OPCODE == `LOAD);
+	assign iType	 =	(`OPCODE == `ADDI || `OPCODE == `LOAD || `OPCODE == `STORE);
+	assign writeReg =	(`OPCODE == `RType ? `R_D : `R_T);
+	assign addrInfo =	 `ADDRINFO;
 
-   // Use your insDecoder module from Homework 3
-
+	always @(*) begin
+	//TODO: add case for ALUop
+	case (`OPCODE)
+		`ADDI:	ALUop = 1;
+		`LOAD:	ALUop = 1;
+		`STORE:	ALUop = 1;
+		`BEQ:	ALUop = 6;
+		`RType:	begin
+			case (`FUNCT)
+				`ADD:	ALUop = 1;
+				`SUB:	ALUop = 2;
+				`AND:	ALUop = 3;
+				`OR:	ALUop = 4;
+				`SLT:	ALUop = 5;
+				default:	ALUop = 3'hx;
+			endcase
+			end
+		default:	ALUop = 3'hx;
+	endcase
+	end
 endmodule
 
 
