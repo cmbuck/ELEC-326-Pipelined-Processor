a0c8873d04a44834bc3e0691cc754d27e06bf89f
wallaceicy06 <seanharger@me.com>
2013-12-14 15:41:38 -0600

    Changed names of the pipereg modules and added some of the functionality

--- processor.v
+++ processor.v
@@ -21,122 +21,114 @@
 
 
 
-module processor(Clk); 
+module processor(Clk);
 
     input Clk;
 // Instantiate modules making up the processor
 
-   wire [11:0] PipeReg1Bus; //connects output of decoder to PipeReg1
-   wire [2:0] aluOp; //connected to intput of ALU
-   wire       ALUsel; //connected to mux before ALU
-   wire       WSel;
-   wire       MemRead; //goes into DataMem
-   wire       MemWrite; //goes into DataMem
-   wire [15:0] AddrInfo_mux_input; //input to MUX before ALU 
-   wire [4:0]  DstReg; //input to DST, output of MUX after rt and rd
-   wire [31:0] TargetAddr; //output of ALU, input of DDR
-   wire [31:0] Result; //output of ALU
-   wire [31:0] rt_mux_input;
-   wire [31:0] rs_mux_input;
-   
-   
+   wire [11:0]  PipeReg1Bus; //connects output of decoder to PipeReg1
+   wire [2:0]   aluOp; //connected to intput of ALU
+   wire         ALUsel; //connected to mux before ALU
+   wire         WSel;
+   wire         MemRead; //goes into DataMem
+   wire         MemWrite; //goes into DataMem
+   wire [15:0]  AddrInfo_mux_input; //input to MUX before ALU 
+   wire [4:0]   DstReg; //input to DST, output of MUX after rt and rd
+   wire [31:0]  TargetAddr; //output of ALU, input of DDR
+   wire [31:0]  Result; //output of ALU
+   wire [31:0]  rt_mux_input;
+   wire [31:0]  rs_mux_input;
+
+
    dataMemory DataMemory(MemRead, MemWrite, Address, StoreValue, LoadValue);
 
    alu ALU(aluOp, operand1, operand2, jmp, br, pc, addrInfo, aluResult, TargetAddr);
-   
 
 
 
-initial begin
-// Initialize all microarchitectural registers
-   
-   end
-     
+
+    initial begin
+    // Initialize all microarchitectural registers
+
+    end
+
 
 
 
 
 endmodule
 
-module pipeReg1(clk, pc, instruction);
-    // The following are contained in this module:
-    // [31:0] pc
-    // [31:0] instruction
-    
-module pipeReg1(clk, pcIn, pcOut, instrIn, instrOut);
+module IFPipeReg(clk, pcIn, pcOut, instrIn, instrOut);
     input clk;
-    input [`INSTR_BIT_LEN - 1:0] pcIn, instrIn;
-    output reg [`INSTR_BIT_LEN - 1:0] pcOut, instrOut;
+    input [31:0] pcIn, instrIn;
+    output reg [31:0] pcOut, instrOut;
 
-    always @(posedge clk)
-    begin
+    always @(posedge clk) begin
         pcOut <= pcIn;
         instrOut <= instrIn;
     end
 endmodule
 
-module pipeReg2();
+module IDPipeReg(clk, pcIn, pcOut, aluSetIn, aluSetOut, wSelIn, wSelOut, aluOpIn, aluOpOut, memWriteIn, memWriteOut, rDataSelIn, rDataSelOut, rWriteIn, rWriteOut, jmpIn, jmpOut, brIn, brOut);
+    input clk, aluSetIn, wSelIn, memWriteIn, rDataSelIn, rWriteIn, jmpIn, brIn;
+    input [31:0] pcIn;
+    input [3:0] aluOpIn;
+    output reg aluSetOut, wSelOut, memWriteOut, rDataSelOut, rWriteOut, jmpOut, brOut;
+    output reg [31:0] pcOut;
+    output [3:0] aluOpOut;
+
+    always @(posedge clk) begin
+        aluSetOut <= aluSetIn;
+        pcOut <= pcIn;
+        wSelOut <= wSelIn;
+        aluSelOut <= aluSelIn;
+        memWriteOut <= memWriteIn;
+        rDataSelOut <= rDataSelIn;
+        rWriteOut <= rWriteIn;
+        jmpOut <= jmpIn;
+        brOut <= brIn;
+    end
+
     // The following are contained in this module:
     // [31:0] pc
     // [10:0] IDCntrl - aluSet, wSel, aluOp(this guy is 3 bits long), memWrite, rDataSel, rWrite, jmp, br
 endmodule
 
-module pipeReg3(wbEnable, memRead, memWrite, iType, isBranch, isJump, alures, rt);
+module EXPipeReg(clk, wbEnable, memRead, memWrite, iType, isBranch, isJump, alures, rt);
     // The following are contained in this module
     // [3:0] ExCntrl - br, memWrite, rDataSel, rWrite
     // [31:0] result
     // [4:0] dst
     // [31:0] ddr
-    input wbEnable, memRead, memWrite, iType, isBranch, isJump;
+    input clk, wbEnable, memRead, memWrite, iType, isBranch, isJump;
     input [31:0] aluRes;
     input [4:0] inRT;
 
     output dataMem;
     output [4:0] outRT;
-    
-    always @(posedge) begin
+
+    always @(posedge clk) begin
         
-    
-        
-    
     end
 endmodule // pipeReg3
 
-module pipeReg4();
+module MEMPipeReg(clk, memWriteIn, memWriteOut, dataIn, dataOut, dstIn, dstOut);
     // memCntrl (just write)
     // [31:0] data
-    // [4:0] dst
+    // [2:0] dst
+    input clk, memWriteIn;
+    input [31:0] dataIn;
+    input [2:0] dstIn;
 
- /*   
+    output reg memWriteOut;
+    output reg [31:0] dataOut;
+    output reg [31:0] dstOut;
 
-module pipeReg4(clk,
-                wbEnableIn, wbEnableOut,
-                memReadIn, memReadOut,
-                memWriteIn, memWriteOut,
-                iTypeIn, iTypeOut,
-                isBranchIn, isBranchOut,
-                isJumpIn, isJumpOut,
-                dataIn, dataOut,
-                dstIn, dstOut);
-
-    input clk;
-    input wbEnableIn, memReadIn, memWriteIn, iTypeIn, isBranchIn, isJumpIn;
-    output reg wbEnableOut, memReadOut, memWriteOut, iTypeOut, isBranchOut, isJumpOut;
-    input [`MEM_WIDTH - 1:0] dataIn;
-    output reg [`MEM_WIDTH - 1:0] dataOut;
-    input [`REG_ADDR_LEN - 1:0] dstIn;
-    output reg [`REG_ADDR_LEN - 1:0] dstOut;
-
-    always @(posedge clk)
-    begin
-        wbEnableOut <= wbEnableIn;
-        memReadOut <= memReadIn;
+    always @(posedge clk) begin
+        dataOut <= dataIn;
+        dstOut <= dstIn;
         memWriteOut <= memWriteIn;
-        iTypeOut <= iTypeIn;
-        isBranchOut <= isBranchIn;
-        isJumpOut <= isJumpIn;
     end
-	*/
 
 endmodule
 
