c1d82e98df1751c7c2b7525a1d6e6c3b0cc4886c
lopezmatt7 <lopezmatt7@gmail.com>
2013-12-15 21:51:02 -0600

    wrote testbench to display pipeRegs

--- processor_tb.v
+++ processor_tb.v
@@ -21,13 +21,17 @@ module processor_TestBench;
    
    
    always @(negedge Clk)  begin
-			   $display("Time:%3d Clk: %d", $time, Clk);
+      $display("Time:%3d Clk:%d\n\n", $time, Clk);
+      $display("ifPipeReg\n PC:%d, Instr:%b\n\n***********************************\n\n", ifPipeReg.pcIn, ifPipeReg.instrIn);
+      $display("idPipeReg\n PC:%d aluSel:%d wSel:%d aluOp:%d memWrite:%d rDataSel:%d rWrite:%d jmp:%d br:%d\n\n***********************************\n\n",
+	       idPipeReg.pcIn, idPipeReg.aluSelIn, idPipeReg.wSelIn, idPipeReg.aluOpIn, idPipeReg.memWriteIn, idPipeReg.rDataSelIn, idPipeReg.rWriteIn, idPipeReg.jmpIn, idPipeReg.brIn);
+      $display("exPipeReg\n br:%d memWrite:%d rDataSel:%d rWrite:%d result:%b dst:%d ddr:%x\n\n***********************************\n\n", exPipeReg.brIn, exPipeReg.memWriteIn, exPipeReg.rDataSelIn, exPipeReg.rWriteIn, exPipeReg.resultIn, exPipeReg.dstIn, exPipeReg.ddrIn);
+      $display("memPipeReg\n memWrite:%d data:%x dst:%d\n\n***********************************\n\n", memPipeReg.memWriteIn, memPipeReg.dataIn, memPipeReg.dstIn); 
    end
 endmodule
 
 
    
-
 
 
 
