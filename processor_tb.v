e6612a0af5e820b43439e9399e3f893a06e834a3
lopezmatt7 <lopezmatt7@gmail.com>
2013-12-15 22:03:27 -0600

    updated display variables to access through two levels: processor.PipeReg.variable

--- processor_tb.v
+++ processor_tb.v
@@ -22,11 +22,15 @@ module processor_TestBench;
    
    always @(negedge Clk)  begin
       $display("Time:%3d Clk:%d\n\n", $time, Clk);
-      $display("ifPipeReg\n PC:%d, Instr:%b\n\n***********************************\n\n", ifPipeReg.pcIn, ifPipeReg.instrIn);
+      $display("ifPipeReg\n PC:%d, Instr:%b\n\n***********************************\n\n", processor.ifPipeReg.pcIn, processor.ifPipeReg.instrIn);
       $display("idPipeReg\n PC:%d aluSel:%d wSel:%d aluOp:%d memWrite:%d rDataSel:%d rWrite:%d jmp:%d br:%d\n\n***********************************\n\n",
-	       idPipeReg.pcIn, idPipeReg.aluSelIn, idPipeReg.wSelIn, idPipeReg.aluOpIn, idPipeReg.memWriteIn, idPipeReg.rDataSelIn, idPipeReg.rWriteIn, idPipeReg.jmpIn, idPipeReg.brIn);
-      $display("exPipeReg\n br:%d memWrite:%d rDataSel:%d rWrite:%d result:%b dst:%d ddr:%x\n\n***********************************\n\n", exPipeReg.brIn, exPipeReg.memWriteIn, exPipeReg.rDataSelIn, exPipeReg.rWriteIn, exPipeReg.resultIn, exPipeReg.dstIn, exPipeReg.ddrIn);
-      $display("memPipeReg\n memWrite:%d data:%x dst:%d\n\n***********************************\n\n", memPipeReg.memWriteIn, memPipeReg.dataIn, memPipeReg.dstIn); 
+	       processor.idPipeReg.pcIn, processor.idPipeReg.aluSelIn, processor.idPipeReg.wSelIn, processor.idPipeReg.aluOpIn, 
+	       processor.idPipeReg.memWriteIn, processor.idPipeReg.rDataSelIn, processor.idPipeReg.rWriteIn, processor.idPipeReg.jmpIn, processor.idPipeReg.brIn);
+      $display("exPipeReg\n br:%d memWrite:%d rDataSel:%d rWrite:%d result:%b dst:%d ddr:%x\n\n***********************************\n\n", processor.exPipeReg.brIn, 
+	       processor.exPipeReg.memWriteIn, processor.exPipeReg.rDataSelIn, processor.exPipeReg.rWriteIn, processor.exPipeReg.resultIn, 
+	       processor.exPipeReg.dstIn, processor.exPipeReg.ddrIn);
+      $display("memPipeReg\n memWrite:%d data:%x dst:%d\n\n***********************************\n\n", processor.memPipeReg.memWriteIn, processor.memPipeReg.dataIn, 
+	       processor.memPipeReg.dstIn); 
    end
 endmodule
 
